f62c452016f1ae9380d0cdfdbdfc3870783f0b0e
Alexander Kennedy <rexer1998@gmail.com>
2019-02-26 20:09:07 -0800

    working video stream

--- routes/uploader.js
+++ routes/uploader.js
@@ -70,53 +70,6 @@ router.post('/', (req, res) => {
   });
 });
 
-router.get("/videoStream.mp4", (req, res) => {
-  //Read all files in the folder
-  fs.readdir(path.join(__dirname, "./uploadedImages/"), (err, files) => {
-
-      //Find most recently uploaded file
-      let numOfFiles = files.length;
-      files[numOfFiles-1];
-
-      let filePath  = "./uploadedImages/" + String(files[numOfFiles-1]);
-      console.log("filePath: " + filePath);
-      res.sendFile(path.join(__dirname, filePath));
-
- 
-  });
-
-  app.get('/video', function(req, res) {
-    const path = './routes/uploadedImages/testVideo.mp4'
-    const stat = fs.statSync(path)
-    const fileSize = stat.size
-    const range = req.headers.range
-    if (range) {
-      const parts = range.replace(/bytes=/, "").split("-")
-      const start = parseInt(parts[0], 10)
-      const end = parts[1] 
-        ? parseInt(parts[1], 10)
-        : fileSize-1
-      const chunksize = (end-start)+1
-      const file = fs.createReadStream(path, {start, end})
-      const head = {
-        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-        'Accept-Ranges': 'bytes',
-        'Content-Length': chunksize,
-        'Content-Type': 'video/mp4',
-      }
-      res.writeHead(206, head);
-      file.pipe(res);
-    } else {
-      const head = {
-        'Content-Length': fileSize,
-        'Content-Type': 'video/mp4',
-      }
-      res.writeHead(200, head)
-      fs.createReadStream(path).pipe(res)
-    }
-  });
-  //res.sendFile(path.join(__dirname, "./uploadedImages/image.png"));
-});
 
 
 module.exports = router;
